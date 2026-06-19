511805c67573edab19582543cc0af59bd1a08192
Baker-C <cdbaker.dev@gmail.com>
2026-06-19 09:00:11 -0700

    fix(frontend): quote TAILSCALE_AUTHKEY to prevent shell splitting

--- SocialMediaAutonomousAgents/frontend/start.sh
+++ SocialMediaAutonomousAgents/frontend/start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 tailscaled --tun=userspace-networking &
 sleep 2
-tailscale up --authkey=$TAILSCALE_AUTHKEY --hostname=render-frontend
+tailscale up --authkey="$TAILSCALE_AUTHKEY" --hostname=render-frontend
 npm start
